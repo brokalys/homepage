d682aee1416ae4a37c47c48487d4cb31885b5e9b
Matiss Janis Aboltins <matiss@mja.lv>
2017-01-21 21:38:34 +0200

    UNCSS

--- publish.js
+++ publish.js
@@ -1,4 +1,36 @@
 var ghpages = require('gh-pages');
 var path = require('path');
  
-ghpages.publish(path.join(__dirname, '_site'));
+
+var uncss = require('uncss')
+var glob = require('glob')
+var fs = require('fs')
+
+var stylesheetLocation = '_site/css/'
+var stylesheetSourceLocation = '_site/css/'
+var stylesheetName = 'main.css'
+
+var jekyllUncss = function() {
+  var css = fs.readFileSync(stylesheetLocation + stylesheetName, 'utf8')
+
+  glob('_site/**/*.html', function(err, files) {
+    if (err) {
+      console.log(err)
+    }
+
+    uncss(files, {
+      raw: css,
+      ignoreSheets:[/(\/css\/|fonts.googleapis)/]
+    }, function(err, output) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      fs.writeFileSync(stylesheetSourceLocation + stylesheetName, output);
+      ghpages.publish(path.join(__dirname, '_site'));
+    })
+  })
+}
+
+jekyllUncss()
